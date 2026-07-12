afe1ab2005bba32924a214e7194509b923850cd2
Abod Hany <afatahhany@gmail.com>
2026-07-12 21:00:30 +0300

    -

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,7 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+import "dotenv/config"
+
+export default {
+  datasource: {
+    url: process.env.DATABASE_URL,
   },
-}
+};
